cdf68d920307242885350552f32032fcac5e5719
nia-bahador <b.pajouheshnia@gmail.com>
2020-04-10 23:48:13 +0430

    Update Clock_Generator.v

--- Clock_Generator.v
+++ Clock_Generator.v
@@ -1,17 +1,17 @@
 //  programmed by BaHaDoR PajouheshNiA
 //  last modification 2020-03-23
 
-module Clock_Generator (clock);
-  output reg clock;
+module Clock_Generator (clock); //module define
+  output reg clock; //input and output section
   
   initial
     begin
-      clock = 1'b 0;
+      clock = 1'b 0; //initialization at time 0
     end
     
   always
     begin
-      #10 clock = ~ clock;
+      #10 clock = ~ clock; //after every 10 unit of time invert the clock
     end
     
 endmodule
